47c8a3b09e0d139b8c96f492ac3463a83b0e986e
Lee <lee1870@purdue.lcl>
2019-06-20 17:11:41 -0400

    First work on Profile Page
    I created a script for the profile page and started with the methods.

--- Assets/Profile.cs
+++ Assets/Profile.cs
@@ -7,12 +7,31 @@ public class Profile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadUserInfo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void LoadUserInfo()
+    {
+        // Load User Icon
+
+        // Load User Description
+    }
+
+    public void UploadButtonPressed()
+    {
+        // Go to Uploads page
+        Debug.Log("Pressed Upload Button in Profile Page");
+    }
+
+    public void NextPage()
+    {
+
+        Debug.Log("Next Page");
     }
 }
