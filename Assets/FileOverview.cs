8646f162e1bbe94fb38745717a5ee8ced56f50d1
GuJialu <msljtacslw@gmail.com>
2019-06-22 19:47:40 -0400

    file overview panel

---
+++ Assets/FileOverview.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileOverview : MonoBehaviour
+{
+    public Image fileCoverImage;
+    public Text authorName;
+    public Text description;
+
+    string fileKey;
+
+    public void LoadFileOverView(string fileKey)
+    {
+
+    }
+
+    public void OpenDetialedPage()
+    {
+
+    }
+}
