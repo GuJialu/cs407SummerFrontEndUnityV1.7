dfd1b23c231dcd0df0f46a261596537bc6c28c60
Lee <lee1870@purdue.lcl>
2019-06-23 16:30:26 -0400

    button list and streamingAssets
    Created dynamic button list of all folders viewable in the list.
    Additionally, created the StreamingAssets Folder, allowing user to access files within their application.

--- Assets/Upload.cs
+++ Assets/Upload.cs
@@ -2,32 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
 
 public class Upload : MonoBehaviour
 {
     public GameObject localFileButtonPrefab;
     public GameObject FileScrollViewContent;
 
-    public GameObject fileName;
+    public InputField uploadTitleTextBox;
+    public InputField fileNameTextBox;
+    public GameObject uploadButton;
+    public GameObject cancelButton;
+    // public GameObject fileName;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject b= Instantiate(localFileButtonPrefab, FileScrollViewContent.transform);
-        b.GetComponentInChildren<Text>().text = "example";
 
+        // GameObject b= Instantiate(localFileButtonPrefab, FileScrollViewContent.transform);
+        // b.GetComponentInChildren<Text>().text = "example";
+        // var myButton = (GameObject)Instantiate(localFileButtonPrefab)
+        // Button button = myButton.getCompo
         // for each folder within the requested directory (provided in WebReq.cs) create a new button.
         // Clicking the button, will fill the two text fields in the panel.
         // https://forum.unity.com/threads/how-to-assign-onclick-for-ui-button-generated-in-runtime.358974/
+
+        // GameObject b = Instantiate(localFileButtonPrefab);
+        // var button = GetComponent<UnityEngine.UI.Button>();
+        // button.onClick.AddListener(() => FileSelected());
+        string filePath = WebReq.objectFolderPath;
+        DirectoryInfo d = new DirectoryInfo(filePath);
+        // string[] files = Directory.GetFiles(filePath);
+        try
+        {
+            foreach (var currDirectory in d.GetDirectories())
+            {
+                Debug.Log(currDirectory.Name);
+                GameObject fileButton = Instantiate(localFileButtonPrefab, FileScrollViewContent.transform);
+                fileButton.GetComponentInChildren<Text>().text = currDirectory.Name;
+                // Hello Moto
+            }
+        }
+        catch
+        {
+            Debug.Log("Failed to get files in directory");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    public void Uploadfiles()
+    {
+        string objectFolderPath = Application.dataPath + "/StreamingAssets/LocalGameFiles/";
+        Debug.Log(objectFolderPath);
+    }
+    public void FileSelected()
+    {
+        // given button title, get upload name from uploadtitlbox
+
+        // then after that, upload the file with the corrosponding information
+
+        // call a method to send data
+    }
+
+    void SendDataToServer(string uploadName)
+    {
+        
+    }
     public void ShowUploadPanel()
     {
         
     }
+
+    
 }
