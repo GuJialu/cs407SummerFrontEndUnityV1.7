89aefed5df85901edec580c7f48ec00608fd4643
Lee <lee1870@purdue.lcl>
2019-06-23 15:03:42 -0400

    Upload Panel
    Edited Script

--- Assets/Upload.cs
+++ Assets/Upload.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Upload : MonoBehaviour
 {
     public GameObject localFileButtonPrefab;
@@ -14,13 +15,22 @@ public class Upload : MonoBehaviour
     {
         GameObject b= Instantiate(localFileButtonPrefab, FileScrollViewContent.transform);
         b.GetComponentInChildren<Text>().text = "example";
-
+        try
+        {
+            foreach (string d in Directory)
+        }
         // for each folder within the requested directory (provided in WebReq.cs) create a new button.
         // Clicking the button, will fill the two text fields in the panel.
         // https://forum.unity.com/threads/how-to-assign-onclick-for-ui-button-generated-in-runtime.358974/
     }
 
     // Update is called once per frame
+
+    void ButtonPressed(string buttonName)
+    {
+        // User has pressed a given button.
+        // Request the page from thebutton that
+    }
     void Update()
     {
         
