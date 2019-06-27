659adbedb83525135db1a428bd45647593e67665
GuJialu <msljtacslw@gmail.com>
2019-06-26 23:36:23 -0400

    Set up for unit tests

---
+++ Assets/Scripts/Testg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject loginAndSignUpPanel = (GameObject)Resources.Load("Prefabs/LoginAndSignUp Panel");
+        //LoginAndSignUp loginAndSignUp = loginAndSignUpPanel.GetComponent<LoginAndSignUp>();
+        Debug.Log(loginAndSignUpPanel.GetComponent<LoginAndSignUp>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Unit Tests/Tests/SignUpLogin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SignUpLogin
+    {
+        LoginAndSignUp loginAndSignUp;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject loginAndSignUpPanel = (GameObject)Resources.Load("Prefabs/LoginAndSignUp Panel");
+            loginAndSignUp = loginAndSignUpPanel.GetComponent<LoginAndSignUp>();
+            Debug.Log(loginAndSignUp);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(loginAndSignUp.gameObject);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SignUpLoginWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+
+            Debug.Log(loginAndSignUp);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator LoginWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+
+            Debug.Log(loginAndSignUp);
+
+            yield return null;
+        }
+    }
+}
