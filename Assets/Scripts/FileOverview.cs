f414a9e64a61a987d01eb8ab2153a7975ea2f016
GuJialu <msljtacslw@gmail.com>
2019-06-27 18:39:32 -0400

    File info download

--- Assets/Scripts/FileOverview.cs
+++ Assets/Scripts/FileOverview.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class FileOverview : MonoBehaviour
@@ -8,12 +11,65 @@ public class FileOverview : MonoBehaviour
     public Image fileCoverImage;
     public Text authorName;
     public Text description;
+    public Text downloads;
+    public Text likes;
+    public Text date;
 
-    string fileKey;
+    string infoDownloadUrl;
 
-    public void LoadFileOverView(string fileKey)
+    public void Init(FileJson fileJson)
     {
+        authorName.text = fileJson.username;
+        downloads.text = fileJson.downloadNum.ToString();
+        likes.text = fileJson.likes.ToString();
+        date.text = fileJson.dateUpdated;
+        infoDownloadUrl = fileJson.infoDownloadUrl;
+        StartCoroutine(RequestDownloadInfoCoro());
+    }
 
+    IEnumerator RequestDownloadInfoCoro()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(infoDownloadUrl))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+                yield break;
+            }
+            else
+            {
+                Debug.Log("file info download complete!");
+                byte[] data = www.downloadHandler.data;
+                UpdateInfo(data);
+            }
+        }
+    }
+
+
+    public void UpdateInfo(byte[] data)
+    {
+        using (MemoryStream zipToOpen = new MemoryStream(data))
+        {
+            using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+            {
+                ZipArchiveEntry desEntry = archive.GetEntry("des.txt");
+                using (StreamReader reader = new StreamReader(desEntry.Open()))
+                {
+                    description.text = reader.ReadToEnd();
+                }
+                ZipArchiveEntry imageEntry = archive.GetEntry("workingspace.PNG");
+                Texture2D Tex2D;
+                Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    imageEntry.Open().CopyTo(ms);
+                    Tex2D.LoadImage(ms.ToArray()); 
+                }
+                fileCoverImage.sprite = Sprite.Create(Tex2D, new Rect(0, 0, Tex2D.width, Tex2D.height), new Vector2(0, 0));
+            }
+        }
     }
 
     public void OpenDetialedPage()
@@ -21,3 +77,4 @@ public class FileOverview : MonoBehaviour
 
     }
 }
+
--- Assets/Scripts/FilePage.cs
+++ Assets/Scripts/FilePage.cs
@@ -20,17 +20,39 @@ class FilePageCache
 [System.Serializable]
 struct FilePageReqJson
 {
-    public string email;
+    public string authorEmail;
     public string sortingMethod;
     public int startRank;
     public int range;
+
+    public FilePageReqJson(string authorEmail, string sortingMethod, int startRank, int range)
+    {
+        this.authorEmail = authorEmail;
+        this.sortingMethod = sortingMethod;
+        this.startRank = startRank;
+        this.range = range;
+    }
+}
+
+[System.Serializable]
+public struct FileJson
+{
+    public string email;
+    public string username;
+    public string fileName;
+    public string type;
+    public string dateUpdated;
+    public int downloadNum;
+    public int likes;
+    public bool anonymous;
+    public string infoDownloadUrl;
 }
 
 [System.Serializable]
 struct FilePageResJson
 {
     public int status;
-    public string err_message;
+    public List<FileJson> file_list;
 }
 
 public class FilePage : MonoBehaviour
@@ -38,35 +60,58 @@ public class FilePage : MonoBehaviour
     public GameObject fileOverviewPanelPrefab;
     public GameObject filePanel;
 
+    string email;
     int currentPageNum;
     int numFiles;
     int numFilesPerPage = 16;
+    int MaxPageNum() { return numFiles / numFilesPerPage + 1;}
+
 
     Queue<FilePageCache> filePageCacheQueue;
+
+    public void Start()
+    {
+        Init("msljtacslw@gmail.com");
+    }
 
     // init the file page, will be called by the parent module(profile, homepage) after instansate a file page
     void Init(string email = null)
     {
-
+        this.email = email;
+        currentPageNum = 1;
+        RequestFiles();
     }
     
-    public void ShowPage(int offset)
+    public void ToPage(int offset)
     {
         int pageNum = currentPageNum + offset;
-        //request files sorted by selected method ranked from currentPageNum to currentPageNum+numFilesPerPage
-        //Change current page
-    }
 
-    public void RequestFilePage(string email = null)
-    {
 
-    }
-
-    IEnumerator RequestProfileCoro(string email)
-    {
-        using (UnityWebRequest www = UnityWebRequest.Post(WebReq.serverUrl + "profile/viewAll", new WWWForm()))
+        if (pageNum > 0/*&&pageNum<MaxPageNum()/*/)
         {
-            byte[] ReqJson = System.Text.Encoding.UTF8.GetBytes(JsonUtility.ToJson(new ProfileReqJson(email)));
+            //do nothing
+            return;
+        }
+
+        currentPageNum = pageNum;
+
+    }
+
+    public void RequestFiles()
+    {
+        StartCoroutine(RequestFilesCoro());
+    }
+
+    IEnumerator RequestFilesCoro()
+    {
+        //Startrank is (currentPageNum-1)*FilesPerPage, Range is files per page
+
+        using (UnityWebRequest www = UnityWebRequest.Post(WebReq.serverUrl + "file/listAll", new WWWForm()))
+        {
+            Debug.Log(JsonUtility.ToJson(new FilePageReqJson(email, "timeASC", currentPageNum - 1, numFilesPerPage)));
+            byte[] ReqJson = System.Text.Encoding.UTF8.GetBytes(
+                JsonUtility.ToJson(new FilePageReqJson(email, "timeASC", currentPageNum-1, numFilesPerPage))
+                );
 
             www.uploadHandler = new UploadHandlerRaw(ReqJson);
             www.SetRequestHeader("Content-Type", "application/json");
@@ -81,9 +126,20 @@ public class FilePage : MonoBehaviour
             {
                 Debug.Log(www.downloadHandler.text);
 
-                ProfileResJson res = JsonUtility.FromJson<ProfileResJson>(www.downloadHandler.text);
-
+                FilePageResJson res = JsonUtility.FromJson<FilePageResJson>(www.downloadHandler.text);
+                Debug.Log(JsonUtility.ToJson(res));
+                CreateFileOverviews(res);
             }
+        }
+    }
+
+    void CreateFileOverviews(FilePageResJson filePageResJson)
+    {
+        foreach(FileJson fileJson in filePageResJson.file_list)
+        {
+            GameObject fileOverviewPanel = Instantiate(fileOverviewPanelPrefab, filePanel.transform);
+            FileOverview fileOverview = fileOverviewPanel.GetComponent<FileOverview>();
+            fileOverview.Init(fileJson);            
         }
     }
 }
