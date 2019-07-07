6a0c9509644e62f2494dc21f07b2d81902eeaa1d
Lee <lee1870@purdue.lcl>
2019-07-07 17:37:39 -0400

    Favorites and Fileoverview Changes

--- Assets/Scripts/Favorites.cs
+++ Assets/Scripts/Favorites.cs
@@ -11,12 +11,15 @@ public class Favorites : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject FavoritesScrollViewContent;
-    public GameObject FavoritesEntryPrefab; // TODO switch with the prefab probably built by Jialu for directory element.
+    public GameObject fileOverviewPanelPrefab;
 
     public GameObject exitButton;
     public GameObject removeFavoriteButton;
 
+
     public Text errorMessage;
+
+    public string endpoint = "profile/viewfavoritefile";
 
     void Start()
     {
@@ -25,8 +28,40 @@ public class Favorites : MonoBehaviour
         // foreach ( var element in request.json )
         // add element to content
         // add listener
+        RequestFavorites();
     }
 
+    public void RequestFavorites()
+    {
+        StartCoroutine(RequestFavoritesCoro());
+    }
+    IEnumerator RequestFavoritesCoro()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(WebReq.serverUrl + endpoint, new WWWForm()))
+        {
+            byte[] ReqJson = System.Text.Encoding.UTF8.GetBytes(
+                JsonUtility.ToJson(new FavoritesPageReqJson(WebReq.email))
+                );
+
+            www.uploadHandler = new UploadHandlerRaw(ReqJson);
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                FilePageResJson res = JsonUtility.FromJson<FilePageResJson>(www.downloadHandler.text);
+                Debug.Log(JsonUtility.ToJson(res));
+                CreateFavoritesOverviews(res);
+            }
+        }
+    }
     public void ElementSelected(string name)
     {
         // TODO, call database for more info, redirect user or pop up with more information.
@@ -44,10 +79,27 @@ public class Favorites : MonoBehaviour
         // TODO create pop up panel.
     }
 
-    
+    void CreateFavoritesOverviews(FilePageResJson filePageResJson)
+    {
+        foreach (FileJson fileJson in filePageResJson.file_list)
+        {
+            GameObject fileOverviewPanel = Instantiate(fileOverviewPanelPrefab, FavoritesScrollViewContent.transform);
+            FileOverview fileOverview = fileOverviewPanel.GetComponent<FileOverview>();
+            fileOverview.Init(fileJson);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+struct FavoritesPageReqJson
+{
+    public string email;
+
+    public FavoritesPageReqJson(string email)
+    {
+        this.email = email;
     }
 }
