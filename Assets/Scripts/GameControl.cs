e2629bea5be8f950469f80c34fb9302b3ac06248
MinGi Kim <voickjole@gmail.com>
2019-07-09 17:02:49 -0400

    Mod download and Dummy game

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class GameControl : MonoBehaviour
 {
+    public GameObject dino;
+    public GameObject rect;
+
+    private GameObject newRect;
+    private Rigidbody2D dinoBody;
+    private float thrust = 300.0f;
+    private float rectmovement = 0.3f;
+    private float myTime = 0.0F;
+    private float genTime = 0.0f;
+    public float nextJump = 1.5F;
+    private float nextGen = 5.0F;
+
+    void Awake()
+    {
+        dinoBody = dino.GetComponent<Rigidbody2D>();
+        newRect = Instantiate(rect, rect.transform.position + Vector3.left * 10.0f, rect.transform.rotation);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,43 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        myTime = myTime + Time.deltaTime;
+        genTime = genTime + Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && myTime > nextJump)
+        {
+            dinoBody.AddForce(dino.transform.up * thrust);
+
+            myTime = 0.0F;
+        }
+
+        if (genTime > nextGen)
+        {
+            newRect = Instantiate(rect, rect.transform.position + Vector3.left * 10.0f, rect.transform.rotation);
+
+            genTime = 0.0f;
+        }
+        else if (nextGen - genTime > 1.0f)
+        {
+            if (newRect != null)
+                newRect.transform.position += Vector3.left * rectmovement;
+        }
+        else
+        {
+            Destroy(newRect);
+        }
+    }
+
+    void FixedUpdate()
+    {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "dino")
+        {
+            Destroy(coll.gameObject);
+        }
     }
 }
