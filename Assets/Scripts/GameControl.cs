8fb1b8ee12ad891169d2274f3edb93311bd84e11
GuJialu <msljtacslw@gmail.com>
2019-07-09 16:02:59 -0400

    Gameloading

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -7,7 +7,8 @@ public class GameControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameConfig.LoadMods(WebReq.objectFolderPath);
+        //GameConfig.LoadMods(WebReq.downloadFolderPath);
     }
 
     // Update is called once per frame
