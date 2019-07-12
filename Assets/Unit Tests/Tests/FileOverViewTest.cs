0b10d3aa9b796ef3e905b35dbf4ff53e9ee4e11e
GuJialu <msljtacslw@gmail.com>
2019-07-12 10:24:23 -0400

    Unit test

--- Assets/Unit Tests/Tests/FileOverViewTest.cs
+++ Assets/Unit Tests/Tests/FileOverViewTest.cs
@@ -18,6 +18,18 @@ namespace Tests
             GameObject fileOverviewPanel = Object.Instantiate((GameObject)Resources.Load("Prefabs/File overview Panel"), o.transform);
             fileOverview = fileOverviewPanel.GetComponent<FileOverview>();
             Debug.Log(fileOverview);
+
+            FileJson fileJson = new FileJson();
+            fileJson.anonymous = false;
+            fileJson.email = "FilePageTest@email.com";
+            fileJson.dateUpdated = "1561736000534";
+            fileJson.fileName = "";
+            fileJson.infoDownloadUrl = new InfoDownloadUrl();
+            fileJson.infoDownloadUrl.status = 200;
+            fileJson.infoDownloadUrl.URL = "";
+            fileJson.key = "FilePageTest@email.com|SpangeBob";
+
+            fileOverview.Init(fileJson);
         }
 
         [UnityTest]
@@ -25,6 +37,16 @@ namespace Tests
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator FileOverViewOpenAuthorProfilePasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            fileOverview.OpenProfilePage();
+
             yield return null;
         }
     }
