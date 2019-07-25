ef5b8fc661e82b1061df1d07d3603c9979fb18f1
GuJialu <msljtacslw@gmail.com>
2019-07-25 16:22:38 -0400

    Main page ui improve

---
+++ Assets/Scripts/MainPage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainPage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToGame()
+    {
+
+    }
+
+    public void ToModEditor()
+    {
+
+    }
+
+    public void ToWorkShop()
+    {
+        SceneManager.LoadScene("WorkShopSence");
+    }
+}
